7e75997c035e35b3a3ebc4eac8708be3a2f087b9
Tawhid Mubashwir <135539633+TawhidMM@users.noreply.github.com>
2026-04-20 20:46:51 +0600

    [backend] tool image names set to docker hub image names

--- backend/app/tools/registry.py
+++ backend/app/tools/registry.py
@@ -1,21 +1,21 @@
 TOOLS = {
     "scribbledom": {
         "type": "docker",
-        "image": "scribbledom:latest",
+        "image": "tawhidmm/st_tools:scribbledom-latest",
         "entrypoint": ["/bin/bash", "/run_scribbledom.sh"],
         "adapter": "app.tools.adapters.scribbledom.ScribbleDomAdapter",
         "config_file": "config.json"
     },
     "staig": {
         "type": "docker",
-        "image": "staig:latest",
+        "image": "tawhidmm/st_tools:staig-latest",
         "entrypoint": ["/bin/bash", "/run_scribbledom.sh"],
         "adapter": "app.tools.adapters.staig.StaigAdapter",
         "config_file": "config.yml"
     },
     "deepst": {
         "type": "docker",
-        "image": "deepst:latest",
+        "image": "tawhidmm/st_tools:deepst-latest",
         "adapter": "app.tools.adapters.deepst.DeepStAdapter",
         "config_file": "config.json"
     }
